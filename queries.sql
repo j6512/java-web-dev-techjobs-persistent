19048ea5b7d60f05b3f9a3115d92939dfd3e024d
justin <justins8287@gmailcom>
2021-03-14 00:35:56 -0600

    Updated Part #1 to list columns and data types from the job table

--- queries.sql
+++ queries.sql
@@ -1,5 +1,9 @@
 ## Part 1: Test it with SQL
 
+SELECT column_name, data_type
+FROM information_schema.columns
+WHERE table_name = 'job';
+
 ## Part 2: Test it with SQL
 
 ## Part 3: Test it with SQL
